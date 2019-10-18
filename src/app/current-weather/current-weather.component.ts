edbd4ef5c4e6249aa37718bf04cfe99b476c83a7
Michael David <mdavidmachacuay@gmail.com>
2019-10-18 10:12:44 -0500

    layout with css grid

--- src/app/current-weather/current-weather.component.ts
+++ src/app/current-weather/current-weather.component.ts
@@ -4,7 +4,7 @@ import {CurrentWeatherService} from "../services/current-weather.service";
 @Component({
   selector: 'app-current-weather',
   templateUrl: './current-weather.component.html',
-  styleUrls: ['./current-weather.component.sass']
+  styleUrls: ['./current-weather.component.scss']
 })
 export class CurrentWeatherComponent implements OnInit {
 
