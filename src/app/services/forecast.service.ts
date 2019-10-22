dd1bde37bd92924d2783611688679f902de29373
Michael David <mdavidmachacuay@gmail.com>
2019-10-22 18:13:13 -0500

    Calculate minimum and maximum temperature per day

--- src/app/services/forecast.service.ts
+++ src/app/services/forecast.service.ts
@@ -3,6 +3,8 @@ import {Observable, Subject} from "rxjs";
 import {Coords} from "../interfaces/coords";
 import {environment} from "../../environments/environment";
 import {HttpClient} from "@angular/common/http";
+import {Weather} from "../interfaces/weather";
+import {map} from "rxjs/operators";
 
 @Injectable({
   providedIn: 'root'
@@ -15,7 +17,7 @@ export class ForecastService {
   endpoint: string = 'https://api.openweathermap.org/data/2.5/forecast';
 
   constructor(private http: HttpClient) {
-    this.weather$ = this.weatherSubject.asObservable();
+    this.weather$ = this.weatherSubject.asObservable().pipe(map(this.structureData));
     this.get({
       lat: 19.427025,
       lon: -99.167665
@@ -23,12 +25,29 @@ export class ForecastService {
   }
 
   structureData(data: any) {
+    let minMaxPerDay = {};
     data.list.forEach(weatherObject => {
       let date = new Date(weatherObject.dt * 1000);
       let hours = date.getHours();
       let month = date.getMonth();
       let day = date.getDate();
+      let key = `${month}-${day}`;
+
+      let tempPerDay: Weather = minMaxPerDay[key] || {
+        minMaxTemp: {}
+      };
+
+      if (!tempPerDay.minMaxTemp.min || (weatherObject.main.temp_min < tempPerDay.minMaxTemp.min)) {
+        tempPerDay.minMaxTemp.min = weatherObject.main.temp_min;
+      }
+      if (!tempPerDay.minMaxTemp.max || (weatherObject.main.temp_max > tempPerDay.minMaxTemp.max)) {
+        tempPerDay.minMaxTemp.max = weatherObject.main.temp_max;
+      }
+
+      minMaxPerDay[key] = tempPerDay;
     });
+
+    return minMaxPerDay;
   }
 
   get(coords: Coords) {
